83e0bf6764310d0829248463094b17e57868536a
pogermano <pogermano@gmail.com>
2021-11-30 12:24:15 -0300

    criar metodo getById para Country

--- src/main/java/com/pogermano/citiesapi/CountryResource.java
+++ src/main/java/com/pogermano/citiesapi/CountryResource.java
@@ -2,12 +2,15 @@ package com.pogermano.citiesapi;
 
 import com.pogermano.citiesapi.countries.Country;
 import com.pogermano.citiesapi.repository.CountryRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Scanner;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/countries")
@@ -20,9 +23,17 @@ public class CountryResource {
     }
 
     @GetMapping
-    public List<Country> countries(){
-
-        return repository.findAll();
+    public Page<Country> countries(Pageable page){
+        return repository.findAll(page);
     }
+    @GetMapping("/{id}")
+    public ResponseEntity<Country> findOne(@PathVariable Long id) {
+        Optional<Country> optional = repository.findById(id);
+        if(optional.isPresent()){
+            return ResponseEntity.ok().body(optional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
 
+    }
 }
